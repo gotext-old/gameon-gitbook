cae463010da7c3e316fb1d0e453b8b03a511f8ec
Erin Schnabel <schnabel@us.ibm.com>
2017-09-14 16:03:30 -0400

    more analytics. :eyeroll:
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- js/analytics.js
+++
@@ -1,5 +0,0 @@
-window.ga('create', 'UA-90113653-2', 'auto', 'gameontext', {allowLinker: true});
-window.ga('gameontext.set', 'dimension1', 'book');
-window.ga('gameontext.require', 'linker');
-window.ga('gameontext.linker:autoLink', ['gameontext.org','blog.gameontext.org','book.gameontext.org']);
-window.ga('gameontext.send', 'pageview');
