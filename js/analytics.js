8cbfab1b5c557e3560d8a526c9d82a2cb96f2ad3
Erin Schnabel <schnabel@us.ibm.com>
2017-09-14 12:49:41 -0400

    custom google analytics
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- js/analytics.js
+++ js/analytics.js
@@ -2,6 +2,6 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga2');
-
-ga2('create', 'UA-90113653-4', 'auto');
+ga2('create', 'UA-90113653-2', 'auto');
+ga('set', 'dimension1', 'book');
 ga2('send', 'pageview');
