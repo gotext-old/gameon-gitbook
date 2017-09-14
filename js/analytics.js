481cac6f60b6d691968916c8eba1b0840f2e2755
Erin Schnabel <schnabel@us.ibm.com>
2017-09-14 13:13:46 -0400

    custom google analytics
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- js/analytics.js
+++ js/analytics.js
@@ -2,6 +2,7 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga2');
-ga2('create', 'UA-90113653-2', 'auto');
-ga2('set', 'dimension1', 'book');
+ga2('create', 'UA-90113653-2', 'auto', {allowLinker: true});
+ga2('require', 'linker');
+ga2('linker:autoLink', ['gameontext.org','blog.gameontext.org','book.gameontext.org']);
 ga2('send', 'pageview');
