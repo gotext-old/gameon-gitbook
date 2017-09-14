ca31cac1c7ab8e2f769ad4e8fc8ee2e31f1ec280
Erin Schnabel <schnabel@us.ibm.com>
2017-09-14 14:04:34 -0400

    more analytics. :eyeroll:
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- js/analytics.js
+++ js/analytics.js
@@ -1,9 +1,5 @@
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga2');
-ga2('create', 'UA-90113653-2', 'auto', {allowLinker: true});
-ga2('set', 'dimension1', 'book');
-ga2('require', 'linker');
-ga2('linker:autoLink', ['gameontext.org','blog.gameontext.org','book.gameontext.org']);
-ga2('send', 'pageview');
+ga('create', 'UA-90113653-2', 'auto', 'gameontext', {allowLinker: true});
+ga('gameontext.set', 'dimension1', 'book');
+ga('gameontext.require', 'linker');
+ga('gameontext.linker:autoLink', ['gameontext.org','blog.gameontext.org','book.gameontext.org']);
+ga('gameontext.send', 'pageview');
